8fc6c0e9e33d6cc28af1ab03dd5f549848d559bc
Veer-Tech Trendings <49147903+speedyj007@users.noreply.github.com>
2019-08-19 22:19:41 +0530

    Add files via upload

---
+++ pattern_3.java
@@ -0,0 +1,42 @@
+
+public class pattern_3{
+public static void main(String []args)
+{
+	String p1 = "*";
+	String p2 = "=";
+
+	System.out.println();
+
+	for(int j = 0;j<=5;j++)
+	{
+
+	for(int i = 0;i<=5;i++ )
+	{
+		System.out.print(" "+p1);
+		
+	}
+	
+	for(int y = 0;y<=8;y++ )
+	{
+		System.out.print(" "+p2);
+		
+	}
+	
+	
+		System.out.println();
+	}
+	
+	
+	for(int j = 0;j<=5;j++)
+	{
+	for(int y = 0;y<=14;y++ )
+	{
+		System.out.print(" "+p2);
+		
+	}
+	System.out.println();
+	}
+	System.out.println();
+	
+	}
+}
